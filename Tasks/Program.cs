d1145735361c048322e8a8e29174d16435d0b825
Iana Glebova <letdrown@gmail.com>
2023-03-25 16:21:00 +0300

    Dellit Hello

---
+++ Tasks/Program.cs
@@ -0,0 +1,90 @@
+﻿Console.WriteLine("Hello, World!");
+
+
+
+
+/*int i = 0;
+
+if (int.TryParse(Console.ReadLine(), out i))
+{
+    Console.WriteLine(i*i);
+}
+else
+{Console.Write("Ops");}*/
+
+/*int a;
+int b;
+Console.WriteLine("Введите число 'a'");
+int.TryParse(Console.ReadLine(), out a);
+Console.WriteLine("Введите число 'b'");
+int.TryParse(Console.ReadLine(), out b);
+
+if (b * b == a)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+
+/*int i;
+Console.WriteLine("Введите цифру от 1 до 7");
+int.TryParse(Console.ReadLine(), out i);
+
+if (i == 1)
+{
+     Console.WriteLine("Понедельник");
+}
+else if (i == 2)
+{
+     Console.WriteLine("Вторник");
+}
+else if (i == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (i == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (i == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (i == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (i == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("Неверно указаны данные. Введите цифру от 1 до 7");
+}*/
+
+
+/* Задача 5
+int a;
+Console.WriteLine("Введите число");
+a = int.Parse(Console.ReadLine());
+int ab = -a;
+
+while (a >= ab)
+{
+    System.Console.WriteLine(-a);
+    a = a - 1;
+
+
+
+
+Задача 6
+int a;
+int b;
+Console.WriteLine("Введите трехзначное число");
+a = int.Parse(Console.ReadLine());
+b = a % 10;
+Console.WriteLine("Последняя цифра: ");
+Console.WriteLine(b);*/
